138380020a46b7d7654dab334b4c84316278297f
Gabriel Robles <gabriel.lucas.asr@gmail.com>
2024-11-10 17:13:54 -0300

    feat: implement CourseController for managing course data operations

---
+++ src/fateczl/docenteapp/controllers/CourseController.java
@@ -0,0 +1,60 @@
+package fateczl.docenteapp.controllers;
+
+import fateczl.csvdb.CsvContext;
+import fateczl.docenteapp.model.Course;
+import fateczl.docenteapp.views.dtos.CourseDto;
+import fateczl.util.Queue;
+
+public class CourseController {
+  private final CsvContext<Course> csvContext;
+
+  public CourseController(CsvContext<Course> csvContext) {
+    this.csvContext = csvContext;
+  }
+
+  public Queue<Course> getAll() {
+    try {
+      return csvContext.readAll();
+    } catch (Exception e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+
+  public void save(CourseDto courseDto) {
+    var course = new Course.Builder()
+                      .code(courseDto.getCode())
+                      .name(courseDto.getName())
+                      .knowledgeArea(courseDto.getKnowledgeArea())
+                      .build();
+
+    try {
+      csvContext.insert(course);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void edit(String id, CourseDto courseDto) {
+    var course = new Course.Builder()
+                      .id(Integer.parseInt(id))
+                      .code(courseDto.getCode())
+                      .name(courseDto.getName())
+                      .knowledgeArea(courseDto.getKnowledgeArea())
+                      .build();
+
+    try {
+      csvContext.update(course, c -> c.getId().equals(course.getId()));
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void delete(Integer id) {
+    try {
+      csvContext.delete(c -> c.getId().equals(id));
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
